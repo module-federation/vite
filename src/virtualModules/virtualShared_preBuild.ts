629799180835669498c6768236525af6c45a756c
zhangHongEn <zhang_h_n@163.com>
2024-08-13 13:42:20 +0800

    fix: #47

--- src/virtualModules/virtualShared_preBuild.ts
+++ src/virtualModules/virtualShared_preBuild.ts
@@ -12,15 +12,15 @@ const emptyNpmDir = resolve(require.resolve("an-empty-js-file"), "../")
 /**
  * The original shared module is proxied by getLoadShareModulePath, and the new shared module is prebuilt here
  */
-const cacheMap2:Record<string, string> = {}
-export function getPreBuildLibPath(pkg: string) : string {
+const cacheMap2: Record<string, string> = {}
+export function getPreBuildLibPath(pkg: string): string {
   if (!cacheMap2[pkg]) cacheMap2[pkg] = `__mf__prebuildwrap_${npmPackageNameToFileName(pkg)}.js`
   const filename = cacheMap2[pkg]
   return filename
 }
 
 function getLocalSharedImportMapFileName() {
-  const {name} = getNormalizeModuleFederationOptions()
+  const { name } = getNormalizeModuleFederationOptions()
   return npmPackageNameToFileName(name) + "_" + "__mf__localSharedImportMap.js"
 }
 // Only npm package name import can trigger pre-build, absolute path cannot
@@ -28,7 +28,7 @@ export function getLocalSharedImportMapId() {
   return `an-empty-js-file/${getLocalSharedImportMapFileName()}`
 }
 export function getLocalSharedImportMapPath() {
-  return resolve(emptyNpmDir,  getLocalSharedImportMapFileName())
+  return resolve(emptyNpmDir, getLocalSharedImportMapFileName())
 }
 export function writeLocalSharedImportMap(pkgList: string[]) {
   writeFileSync(getLocalSharedImportMapPath(), `
@@ -46,16 +46,18 @@ export function writeLocalSharedImportMap(pkgList: string[]) {
 /**
  * generate loadShare virtual module
  */
-const cacheMap1:Record<string, string> = {}
-export function getLoadShareModulePath(pkg: string) : string {
-  const {name} = getNormalizeModuleFederationOptions()
+const cacheMap1: Record<string, string> = {}
+export function getLoadShareModulePath(pkg: string): string {
+  const { name } = getNormalizeModuleFederationOptions()
   if (!cacheMap1[pkg]) cacheMap1[pkg] = npmPackageNameToFileName(name) + "_" + `__mf__loadShare_${npmPackageNameToFileName(pkg)}.js`
   const filename = cacheMap1[pkg]
   return resolve(emptyNpmDir, filename)
 }
 export function writeLoadShareModule(pkg: string, shareItem: ShareItem, command: string) {
-  
+
   writeFileSync(getLoadShareModulePath(pkg), `
+    // dev uses dynamic import to separate chunks
+    ${command !== "build" ? `;() => import(${JSON.stringify(pkg)}).catch(() => {});` : ''}
     const {loadShare} = require("@module-federation/runtime")
     const res = loadShare(${JSON.stringify(pkg)}, {
     customShareInfo: {shareConfig:{
