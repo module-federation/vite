bf5547356c086e2c8f9c4bf3d0a5a862ba57f265
zhangHongEn <zhang_h_n@163.com>
2024-09-17 18:08:55 +0800

    fix: test #89

--- lib/utils/getLocalSharedImportMap_windows.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function getRemoteEntryPath_windows(): string;
-export declare function writeRemoteEntry_windows(content: string): void;
--- src/virtualModules/virtualRemoteEntry.ts
+++ src/virtualModules/virtualRemoteEntry.ts
@@ -15,21 +15,21 @@ export function addUsedShares(pkg: string) {
 // *** Expose locally provided shared modules here
 const localSharedImportMapModule = new VirtualModule("localSharedImportMap")
 export function getLocalSharedImportMapPath() {
-  if (process.platform === "win32") {
+  // if (process.platform === "win32") {
     return getLocalSharedImportMapPath_windows()
-  }
-  return localSharedImportMapModule.getPath()
+  // }
+  // return localSharedImportMapModule.getPath()
 }
 let prevSharedCount: number | undefined
 export function writeLocalSharedImportMap() {
   const sharedCount = getUsedShares().size
   if (prevSharedCount !== sharedCount) {
     prevSharedCount = sharedCount
-    if (process.platform === "win32") {
+    // if (process.platform === "win32") {
       writeLocalSharedImportMap_windows(generateLocalSharedImportMap())
-    } else {
-      localSharedImportMapModule.writeSync(generateLocalSharedImportMap(), true)
-    }
+    // } else {
+    //   localSharedImportMapModule.writeSync(generateLocalSharedImportMap(), true)
+    // }
   }
 }
 export function generateLocalSharedImportMap() {
