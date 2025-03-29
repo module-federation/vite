5f356d02aa7e5ee268e039314b6a9d8215c2cc80
Oleksandr Sabov <alexsabov@gmail.com>
2025-03-29 18:55:39 +0100

    Rename variable

--- src/plugins/pluginProxyRemoteEntry.ts
+++ src/plugins/pluginProxyRemoteEntry.ts
@@ -60,11 +60,11 @@ export default function (): Plugin {
             typeof viteConfig.server?.host === 'string' && viteConfig.server.host !== '0.0.0.0'
               ? viteConfig.server.host
               : 'localhost';
-          const originFromConfig = `//${host}:${viteConfig.server?.port}`;
+          const origin = `//${host}:${viteConfig.server?.port}`;
           const alwaysReadOriginFromConfig =
             typeof options.dev === 'object' && options.dev.alwaysReadOriginFromConfig;
           return `
-          const origin = ${alwaysReadOriginFromConfig ? '' : 'window ? window.origin : '}"${originFromConfig}"
+          const origin = ${alwaysReadOriginFromConfig ? '' : 'window ? window.origin : '}"${origin}"
           const remoteEntryPromise = await import(origin + "${viteConfig.base + options.filename}")
           // __tla only serves as a hack for vite-plugin-top-level-await. 
           Promise.resolve(remoteEntryPromise)
