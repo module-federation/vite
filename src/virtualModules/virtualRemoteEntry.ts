065f845c6e71d1870d7ebd19a0408c9d73ac4d87
aqureshi02 <asim.qureshi@crowdstrike.com>
2025-08-26 22:51:26 -0400

    fix: sort all shared items to create deterministic builds when hashes are used

--- src/virtualModules/virtualRemoteEntry.ts
+++ src/virtualModules/virtualRemoteEntry.ts
@@ -40,6 +40,7 @@ export function generateLocalSharedImportMap() {
   return `
     const importMap = {
       ${Array.from(getUsedShares())
+        .sort()
         .map(
           (pkg) => `
         ${JSON.stringify(pkg)}: async () => {
