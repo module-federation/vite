1906d5dbbdd309f469b3a79a7e33076b363088ff
tibimunteanu <tmunteanu@sandtech.com>
2026-09-03 12:24:42 +0300

    fix: ignore type-only imports in expose preflight

---
+++ integration/fixtures/nested-remote-transitive/type-only-import.ts
@@ -0,0 +1,4 @@
+import type { TypedDocumentNode } from '@graphql-typed-document-node/core';
+import { helper } from 'remoteA/shared/helpers';
+
+export const document = helper as unknown as TypedDocumentNode;
